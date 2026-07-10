90c48226aae9ae97691d5ca35049a8ee755f1244
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-11 02:38:14 +0530

    project done

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-def get_requirements()->List[str]:
-    requirements_list:List[str]=[]
-    return  requirements_list
+
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str = "requirements.txt") -> List[str]:
+    """
+    Read requirements from requirements.txt.
+    """
+
+    requirements = []
+
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
 
 setup(
-    name="Currency-Convertor-Tool",
-    version="0.0.1",
+    name="ai_currency_converter",
+    version="1.0.0",
     author="Shravan Kumar Pandey",
     author_email="shravankumarpandey825412@gmail.com",
+    description="Production Ready AI Currency Converter using Gemini and Exchange Rate API",
     packages=find_packages(),
     install_requires=get_requirements(),
-
+    python_requires=">=3.10",
 )
